809fb40e9e0a914470fb1668c95d0df6a2cd9c41
Ivan Coronado <themaster.coronado1234@gmail.com>
2023-09-22 15:16:57 -0700

    IvanLinkedList v1.0

--- C++/PracticingCode/linkedList/linkedList.cpp
+++ C++/PracticingCode/linkedList/linkedList.cpp
@@ -2,67 +2,146 @@
 
 class Nodo{
     public:
-        int dato;
-        Nodo *siguiente;
+        int data;
+        Nodo *next;
+        // Nodo(int info){
+        //     data = info;
+        //     next = nullptr;
+        // }
+    
 };
 
-class linkedList{
+class LinkedList{
     public:
+
+        // Crear la referencia al primer nodo (head)
         Nodo *head;
-        linkedList(){
+        LinkedList(){
             head = NULL;
         }
-        void insertar(int dato){
-            Nodo *nuevo = new Nodo();
-            nuevo->dato = dato;
-            nuevo->siguiente = head;
-            head = nuevo;
+
+        //crear un nodo
+        void create(int data){
+            Nodo *nodo = new Nodo();
+            nodo->data = data;
+            nodo->next = head;
+            head = nodo;
         }
 
-        Nodo* buscar(int dato){
+        // updatear un nodo
+        void update(int data, int nodoData){
             Nodo *temp = head;
             while(temp != NULL){
-                if(temp->dato == dato){
+                if(temp->data == data){
+                    temp->data = nodoData;
+                    return;
+                }
+                temp = temp->next;
+            }
+        }
+
+        //borrar un nodo
+        void del(int data){
+            Nodo *temp = head;
+            Nodo *temp2 = head;
+            while(temp != NULL){
+                if(temp->data == data){
+                    temp2->next = temp->next;
+                    delete temp;
+                    return;
+                }
+                temp2 = temp;
+                temp = temp->next;
+            }
+        }
+
+        //buscar un nodo
+        Nodo* read(int data){
+            Nodo *temp = head;
+            while(temp != NULL){
+                if(temp->data == data){
                     return temp;
                 }
-                temp = temp->siguiente;
+                temp = temp->next;
             }
             return NULL;
         }
 
+        //mostrar la list por consola
         void mostrar(){
             Nodo *temp = head;
             while(temp != NULL){
-                std::cout<<temp->dato<<" ";
-                temp = temp->siguiente;
+                std::cout<<temp->data<<" ";
+                temp = temp->next;
             }
             std::cout<<std::endl;
         }
 };
 
 
-
 int main(){
 
-    linkedList lista;
-    lista.insertar(5);
-    lista.insertar(10);
-    lista.insertar(15);
-    lista.insertar(20);
-    lista.insertar(25);
-    lista.insertar(30);
-    lista.insertar(35);
-    lista.insertar(40);
+    // Crear una list en cadena
+    LinkedList list;
+    int opt;
 
-    lista.mostrar();
+    // Menu de opciones para probar la Linked List
+    while(opt != 0){
 
-    Nodo *buscado = lista.buscar(20);
-    if(buscado != NULL){
-        std::cout<<"El dato fue encontrado"<<std::endl;
-    }else{
-        std::cout<<"El dato no fue encontrado"<<std::endl;
+        std::cout << "1. Crear un nodo\n2. Actualizar un nodo\n3. Borrar un nodo\n4. Buscar un nodo\n5. Mostrar la list\n0. Salir\nOpcion: ";
+        std::cin>>opt;
+
+        // Crear un nodo en la list
+        if(opt == 1){
+            int data;
+            std::cout<<"Ingrese el dato: ";
+            std::cin>>data;
+            list.create(data);
+
+        // Updatear un nodo en la list
+        }else if(opt == 2){
+            int data, nodoData;
+            std::cout<<"Ingrese el dato a actualizar: ";
+            std::cin>>data;
+            std::cout<<"Ingrese el nodo data: ";
+            std::cin>>nodoData;
+            list.update(data, nodoData);
+
+        // Borrar un nodo en la list
+        }else if(opt == 3){
+            int data;
+            std::cout<<"Ingrese el dato a borrar: ";
+            std::cin>>data;
+            list.del(data);
+
+        // Buscar un nodo en la list
+        }else if(opt == 4){
+            int data;
+            std::cout<<"Ingrese el dato a buscar: ";
+            std::cin>>data;
+            Nodo *searched = list.read(data);
+            if(searched != NULL){
+                std::cout<<"El dato fue encontrado"<<std::endl;
+            }else{
+                std::cout<<"El dato no fue encontrado"<<std::endl;
+            }
+
+        // Mostrar la list
+        }else if(opt == 5){
+            list.mostrar();
+
+        // Salir del programa
+        }else if(opt == 0){
+            std::cout<<"list Final: "<<std::endl;
+            
+        }else{
+            std::cout<<"Opcion invalida"<<std::endl;
+
+        }
+
     }
 
+    list.mostrar();
 
     return 0;
 }
