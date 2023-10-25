e23fafe55b00d893bcf6bbfe45db7f570e420ea5
DanielBrMz <A01254805@tec.mx>
2023-10-24 20:15:35 -0700

    3.4 the end is near

--- C++/3.4/main.cpp
+++ C++/3.4/main.cpp
@@ -1,39 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 #include "PriorityQueue/priority_queue.h"
 
 int main() {
-    std::ifstream inFile("input1.txt");
-    if (!inFile) {
-        std::cerr << "No se pudo abrir el archivo input1.txt" << std::endl;
-        return 1;
+    for (int i = 1; i <= 4; i++) {
+        std::stringstream ss;
+        ss << "input" << i << ".txt";
+        std::string filename = ss.str();
+
+        std::ifstream inFile(filename);
+        if (!inFile) {
+            std::cerr << "No se pudo abrir el archivo " << filename << std::endl;
+            continue;
+        }
+
+        int N;
+        inFile >> N;
+
+        priority_queue pq;
+        for (int j = 0; j < N; j++) {
+            int size;
+            inFile >> size;
+            pq.push(size);
+        }
+
+        int totalComparisons = 0;
+        while (pq.size() > 1) {
+            int first = pq.top();
+            pq.pop();
+            int second = pq.top();
+            pq.pop();
+
+            totalComparisons += first + second;
+            pq.push(first + second);
+        }
+
+        std::cout << "Total de comparaciones para " << filename << ": " << totalComparisons << std::endl;
+
+        inFile.close();
     }
-
-    int N;
-    inFile >> N;
-
-    priority_queue pq;
-    for (int i = 0; i < N; i++) {
-        int size;
-        inFile >> size;
-        pq.push(size);
-    }
-
-    int totalComparisons = 0;
-    while (pq.size() > 1) {
-        int first = pq.top();
-        pq.pop();
-        int second = pq.top();
-        pq.pop();
-
-        totalComparisons += first + second;
-        pq.push(first + second);
-    }
-
-    std::cout << totalComparisons << std::endl;
-
-    inFile.close();
 
     return 0;
 }
