0e5213eb77e543575615d6258cff38b7d4ced8c8
DanielBrMz <A01254805@tec.mx>
2023-11-11 23:05:12 -0700

    dev: 4.1 the end is near

--- C++/4.1/Grafo/Grafo.cpp
+++ C++/4.1/Grafo/Grafo.cpp
@@ -1,82 +1,89 @@
 #include "Grafo.h"
 
+// Constructor: Tiempo O(V^2), Espacio O(V^2)
 Grafo::Grafo(int V) {
-    this->V = V;
-    matrizAdj.resize(V, std::vector<int>(V, 0));
+    this->V = V; // O(1)
+    matrizAdj.resize(V, std::vector<int>(V, 0)); // O(V^2)
 }
 
+// loadGraph: Tiempo O(E), Espacio O(V + E)
 void Grafo::loadGraph(int n, int m, std::vector<std::pair<int, int>>& edges) {
-    this->V = n;
-    matrizAdj.resize(V, std::vector<int>(V, 0));
-    for (auto edge : edges) {
+    this->V = n; // O(1)
+    matrizAdj.resize(V, std::vector<int>(V, 0)); // O(V^2)
+    for (auto edge : edges) { // O(E)
         int v = edge.first;
         int w = edge.second;
-        matrizAdj[v][w] = 1;
-        matrizAdj[w][v] = 1;
-        listaAdj[v].push_back(w);
-        listaAdj[w].push_back(v);
+        matrizAdj[v][w] = 1; // O(1)
+        matrizAdj[w][v] = 1; // O(1)
+        listaAdj[v].push_back(w); // O(1)
+        listaAdj[w].push_back(v); // O(1)
     }
 }
 
+// addEdge: Tiempo O(1), Espacio O(1)
 void Grafo::addEdge(int v, int w) {
-    matrizAdj[v][w] = 1;
-    matrizAdj[w][v] = 1;
-    listaAdj[v].push_back(w);
-    listaAdj[w].push_back(v);
+    matrizAdj[v][w] = 1; // O(1)
+    matrizAdj[w][v] = 1; // O(1)
+    listaAdj[v].push_back(w); // O(1)
+    listaAdj[w].push_back(v); // O(1)
 }
 
+// printGraph: Tiempo O(V^2 + E), Espacio O(1)
 void Grafo::printGraph(){
-    std::cout << "Matriz de adyacencia:\n";
-    for (int i = 0; i < V; i++) {
-        std::cout << i << ": ";
-        for (int j = 0; j < V; j++) {
-            std::cout << matrizAdj[i][j] << " ";
+    std::cout << "Matriz de adyacencia:\n"; // O(1)
+    for (int i = 0; i < V; i++) { // O(V)
+        std::cout << i << ": "; // O(1)
+        for (int j = 0; j < V; j++) { // O(V)
+            std::cout << matrizAdj[i][j] << " "; // O(1)
         }
-        std::cout << "\n";
+        std::cout << "\n"; // O(1)
     }
 
-    std::cout << "\nLista de adyacencia:\n";
-    for (auto i : listaAdj) {
-        std::cout << i.first << ": ";
-        for (auto j : i.second) {
-            std::cout << j << " ";
+    std::cout << "\nLista de adyacencia:\n"; // O(1)
+    for (auto i : listaAdj) { // O(V)
+        std::cout << i.first << ": "; // O(1)
+        for (auto j : i.second) { // O(E/V)
+            std::cout << j << " "; // O(1)
         }
-        std::cout << "\n";
+        std::cout << "\n"; // O(1)
     }
 }
 
+// DFS: Tiempo O(V + E), Espacio O(V)
 void Grafo::DFS(int inicio) {
-    std::vector<bool> visited(V, false);
-    DFSUtil(inicio, visited);
+    std::vector<bool> visited(V, false); // O(V)
+    DFSUtil(inicio, visited); // O(V + E)
 }
 
+// DFSUtil: Tiempo O(V + E), Espacio O(V)
 void Grafo::DFSUtil(int v, std::vector<bool>& visited) {
-    visited[v] = true;
-    std::cout << v << " ";
+    visited[v] = true; // O(1)
+    std::cout << v << " "; // O(1)
 
-    for (int i = 0; i < V; i++) {
-        if (matrizAdj[v][i] == 1 && !visited[i]) {
-            DFSUtil(i, visited);
+    for (int i = 0; i < V; i++) { // O(V)
+        if (matrizAdj[v][i] == 1 && !visited[i]) { // O(1)
+            DFSUtil(i, visited); // O(V + E)
         }
     }
 }
 
+// BFS: Tiempo O(V + E), Espacio O(V)
 void Grafo::BFS(int inicio) {
-    std::vector<bool> visited(V, false);
-    std::queue<int> queue;
+    std::vector<bool> visited(V, false); // O(V)
+    std::queue<int> queue; // O(1)
 
-    visited[inicio] = true;
-    queue.push(inicio);
+    visited[inicio] = true; // O(1)
+    queue.push(inicio); // O(1)
 
-    while (!queue.empty()) {
-        int v = queue.front();
-        std::cout << v << " ";
-        queue.pop();
+    while (!queue.empty()) { // O(V)
+        int v = queue.front(); // O(1)
+        std::cout << v << " "; // O(1)
+        queue.pop(); // O(1)
 
-        for (auto i : listaAdj[v]) {
-            if (!visited[i]) {
-                queue.push(i);
-                visited[i] = true;
+        for (auto i : listaAdj[v]) { // O(E/V)
+            if (!visited[i]) { // O(1)
+                queue.push(i); // O(1)
+                visited[i] = true; // O(1)
             }
         }
     }
--- C++/4.1/appGrafo.cpp
+++ C++/4.1/appGrafo.cpp
@@ -1,6 +1,36 @@
 #include "Grafo/Grafo.h"
 #include <vector>
 
+/*
+    Este programa crea dos grafos y realiza un recorrido DFS y BFS a partir de un nodo inicial.
+    El programa implementa la clase Grafo, que se encuentra en la carpeta Grafo.
+    Este grafo se forma a partir de una matriz de adyacencia y una lista de adyacencia.
+*/
+
+/*
+    Complejidad temporal:
+        - Grafo (constructor): O(V^2)
+        - loadGraph: O(V + E)
+        - addEdge: O(1)
+        - printGraph: O(V^2)
+        - DFS: O(V + E)
+        - BFS: O(V + E)
+    Complejidad espacial:
+        - Grafo (constructor): O(V^2)
+        - loadGraph: O(V + E)
+        - addEdge: O(1)
+        - printGraph: O(V^2)
+        - DFS: O(V)
+        - BFS: O(V)
+
+        Autor: Daniel Alfredo Barreras Meraz
+        Matricula: A01254805
+        Fecha: 12/11/2023
+*/
+
+#include "Grafo/Grafo.h"
+#include <vector>
+
 int main() {
     // Crear el primer grafo
     // 0---1   2
@@ -35,6 +65,40 @@ int main() {
     g2.DFS(0);
     std::cout << "\nRecorrido BFS a partir del nodo 0: ";
     g2.BFS(0);
+
+    std::cout << "\n";
+
+    // Crear el tercer grafo
+    // 0---1---2
+    // |   |   |
+    // 3---4---5
+    int numVertices3 = 6;
+    std::vector<std::pair<int, int>> arcos3 = {{0, 1}, {1, 2}, {0, 3}, {1, 4}, {2, 5}, {3, 4}, {4, 5}};
+    Grafo g3(numVertices3);
+    g3.loadGraph(numVertices3, arcos3.size(), arcos3);
+    g3.printGraph();
+    std::cout << "Recorrido DFS a partir del nodo 0: ";
+    g3.DFS(0);
+    std::cout << "\nRecorrido BFS a partir del nodo 0: ";
+    g3.BFS(0);
+
+    std::cout << "\n";
+
+    // Crear el cuarto grafo
+    // 0---1
+    // |   |
+    // 2---3---4
+    //     |   |
+    //     5---6
+    int numVertices4 = 7;
+    std::vector<std::pair<int, int>> arcos4 = {{0, 1}, {1, 3}, {0, 2}, {2, 3}, {3, 4}, {3, 5}, {4, 6}, {5, 6}};
+    Grafo g4(numVertices4);
+    g4.loadGraph(numVertices4, arcos4.size(), arcos4);
+    g4.printGraph();
+    std::cout << "Recorrido DFS a partir del nodo 0: ";
+    g4.DFS(0);
+    std::cout << "\nRecorrido BFS a partir del nodo 0: ";
+    g4.BFS(0);
 
     return 0;
 }
