ab618fe1c7673705cc683ad5e817b862cecbf878
DanielBrMz <a01254805@tec.mx>
2023-11-25 14:43:57 -0700

    4.3 reflex

--- C++/4.3/main.cpp
+++ C++/4.3/main.cpp
@@ -2,6 +2,31 @@
 #include <fstream>
 #include "Grafo/Grafo.h"
 
+/*
+  Este programa utiliza la clase Grafo para resolver el problema de la siguiente manera:
+    - Para cada caso de prueba:
+        - Lee el número de conexiones
+        - Lee las conexiones
+        - Lee el número de consultas
+        - Para cada consulta:
+        - Lee el puerto inicial y el MNP
+        - Calcula e imprime el número de puertos no alcanzables
+  Se utiliza la estructura de datos Grafo para representar el grafo de conexiones y la función unreachablePorts para calcular el número de puertos no alcanzables.
+    Complejidad Temporal de algoritmos utilizados:
+        - addEdge: O(1)
+        - unreachablePorts (BFS): O(V + E)
+    Complejidad Espacial de algoritmos utilizados:
+        - addEdge: O(1)
+        - unreachablePorts: O(V)
+    Donde V es el número de vértices (puertos) y E es el número de aristas (conexiones)
+*/
+
+/*
+  Autor: Daniel Alfredo Barreras Meraz
+  Matrícula: A01254805
+  Fecha: 13 de noviembre de 2023
+*/
+
 void processFile(std::ifstream& inputFile, int i) {
     if (!inputFile.is_open()) {
         std::cout << "Unable to open file input" + std::to_string(i) + ".txt" << std::endl;
