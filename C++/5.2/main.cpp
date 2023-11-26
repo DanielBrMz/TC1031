24dc70d4fa50121054f3760d58b356091a571753
DanielBrMz <a01254805@tec.mx>
2023-11-25 18:46:54 -0700

    5.2 The end is near

--- C++/5.2/main.cpp
+++ C++/5.2/main.cpp
@@ -3,29 +3,33 @@
 #include <fstream>
 
 int main() {
-    HashTable hash_table;
-    std::ifstream input_file("input1.txt");
-    if (!input_file) {
-        std::cerr << "No se pudo abrir el archivo input1.txt" << std::endl;
-        return 1;
-    }
-    int m, n;
-    input_file >> m >> n;
-    for (int i = 0; i < m; ++i) {
-        std::string word;
-        int value;
-        input_file >> word >> value;
-        hash_table.insert(word, value);
-    }
-    input_file.ignore();  // Ignora el salto de línea después del último valor
-    for (int i = 0; i < n; ++i) {
-        std::string job_description, line;
-        while (std::getline(input_file, line) && line != ".") {
-            job_description += line + " ";
+    for (int t = 1; t <= 4; ++t) {
+        std::ifstream archivoEntrada("input" + std::to_string(t) + ".txt");
+        if (!archivoEntrada) {
+            std::cerr << "No se pudo abrir el archivo " << t << std::endl;
+            return 1;
         }
-        int salary = hash_table.calculate_salary(job_description);
-        std::cout << salary << std::endl;
+        HashTable tablaHash;
+        int m, n;
+        archivoEntrada >> m >> n;
+        for (int i = 0; i < m; ++i) {
+            std::string palabra;
+            int valor;
+            archivoEntrada >> palabra >> valor;
+            tablaHash.insert(palabra, valor);
+        }
+        archivoEntrada.ignore();  // Ignora el salto de línea después del último valor
+        std::cout << "Caso de prueba " << t << ":" << std::endl;
+        for (int i = 0; i < n; ++i) {
+            std::string descripcionPuesto, linea;
+            while (std::getline(archivoEntrada, linea) && linea != ".") {
+                descripcionPuesto += linea + " ";
+            }
+            int salario = tablaHash.calculate_salary(descripcionPuesto);
+            std::cout << salario << std::endl;
+        }
+        std::cout << std::endl;
+        archivoEntrada.close();
     }
-    input_file.close();
     return 0;
 }
